333de4356cddad44ee76f3a7b459d0c447102693
Azarenkov Georgy <gosha.azarenkov0403@gmail.com>
2023-07-03 11:31:25 +0300

    Base

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ts-issue-template
+## Окружение
+
+### Общее
+1. npm 9.5.1
+2. node 18.16.0
+3. typescript 5.1.6
+
+### IDE
+1. WebStorm 2023.1.3 (Build #WS-231.9161.29, built on June 15, 2023)
+2. Non-Bundled Plugins:
+   1. com.github.copilot (1.2.8.2631)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+function Decorator<C extends object>(lambda: (c: C) => any) {
+    return function (target: C, propertyKey: string): void {
+
+    }
+}
+
+class TestClass {
+    @Decorator((c) => c.test)
+    public test: string;
+}
